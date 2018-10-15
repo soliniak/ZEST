1e7d5ff0bf3c1c1eb60d1abce9986a7067f08d03
Rafi <soliniak@gmail.com>
2018-10-15 21:44:44 +0200

    Desktop view

---
+++ Gulpfile.js
@@ -0,0 +1,62 @@
+const gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  browserSync = require("browser-sync"),
+  autoprefixer = require("gulp-autoprefixer"),
+  minify = require("gulp-minifier");
+
+const baseDirVar = "./src/";
+
+// Static server
+gulp.task("browserSync", function() {
+  browserSync.init({
+    server: {
+      baseDir: baseDirVar
+    }
+  });
+});
+
+gulp.task("styles", function() {
+  gulp
+    .src(baseDirVar + "style/**/*.sass")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest(baseDirVar + "/style/"));
+});
+
+gulp.task("prefix", function() {
+  gulp
+    .src("style/**/*.css")
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"],
+        cascade: false
+      })
+    )
+    .pipe(gulp.dest("dist"));
+});
+
+gulp.task("mini", function() {
+  return gulp
+    .src("**/*")
+    .pipe(
+      minify({
+        minify: true,
+        collapseWhitespace: true,
+        conservativeCollapse: true,
+        minifyJS: true,
+        minifyCSS: true,
+        getKeptComment: function(content, filePath) {
+          var m = content.match(/\/\*![\s\S]*?\*\//gim);
+          return (m && m.join("\n") + "\n") || "";
+        }
+      })
+    )
+    .pipe(gulp.dest("example/dest"));
+});
+
+//Watch task
+gulp.task("watch", ["browserSync", "styles"], function() {
+  gulp.watch(baseDirVar + "style/**/*.sass", ["styles"]);
+  gulp.watch(baseDirVar + "style/**/*.sass", browserSync.reload);
+  gulp.watch(baseDirVar + "*.html", browserSync.reload);
+  gulp.watch(baseDirVar + "scripts/**/*.js", browserSync.reload);
+});
---
+++ src/scripts/script.js
@@ -0,0 +1,113 @@
+"use strict";
+
+// variables
+const fLinkPrev = document.querySelector(".link-prev"), // footer link "prev"
+  fLinkNext = document.querySelector(".link-next"), // title link "next"
+  tLinkPrev = document.querySelectorAll(".title-link-prev"), // title link "prev"
+  tLinkNext = document.querySelectorAll(".title-link-next"), // footer link "next"
+  linkHome = document.querySelector(".link-home"), // logo link to "home"
+  sections = document.querySelectorAll("section"), // all sections
+  mainText = document.querySelectorAll(".main_title"), // all main text's
+  sideTitleLeft = document.querySelectorAll(".side_title-left"),
+  sideTitleRight = document.querySelectorAll(".side_title-right"),
+  content = document.querySelectorAll(".content"),
+  menuButton = document.querySelector(".menu-button"),
+  main = document.querySelector(".main"),
+  mainMenu = document.querySelector(".main_menu"),
+  mainLinks = document.querySelectorAll(".main-link");
+
+let current = 0; // current section
+
+// home link
+linkHome.addEventListener("click", () => {
+  swipe(0);
+});
+
+// handle clicking "prev" link in footer
+fLinkPrev.addEventListener("click", () => {
+  swipe("left");
+});
+
+// handle clicking "next" link in footer
+fLinkNext.addEventListener("click", () => {
+  swipe("right");
+});
+
+// handle clicking title link - go to previous section
+[].forEach.call(tLinkPrev, link => {
+  link.addEventListener("click", () => {
+    swipe("left");
+  });
+});
+
+// handle clicking title link - go to previous section
+[].forEach.call(tLinkNext, link => {
+  link.addEventListener("click", () => {
+    swipe("right");
+  });
+});
+
+// start first animation after page's loaded
+window.addEventListener("DOMContentLoaded", () => {
+  animateCurrentText(0); // trigger text animation
+});
+
+// animate current section's title and side text links
+const animateCurrentText = current => {
+  [].forEach.call(mainText, text => {
+    text.classList.remove("main-title-animation");
+  });
+  [].forEach.call(sideTitleLeft, text => {
+    text.classList.remove("left-title-animation");
+  });
+  [].forEach.call(sideTitleRight, text => {
+    text.classList.remove("right-title-animation");
+  });
+  mainText[current].classList.add("main-title-animation");
+  sideTitleLeft[current].classList.add("left-title-animation");
+  sideTitleRight[current].classList.add("right-title-animation");
+};
+
+// open / hide menu
+menuButton.addEventListener("click", () => {
+  mainMenu.classList.toggle("main_menu-active");
+  main.classList.toggle("main-active");
+});
+// hide menu
+const hideMenu = () => {
+  mainMenu.classList.remove("main_menu-active");
+  main.classList.remove("main-active");
+};
+
+// place sections next to each other in row
+[].forEach.call(content, (el, i) => {
+  el.style.left = i * 100 + "%";
+});
+
+// display selected section
+const swipeSection = current => {
+  [].forEach.call(content, el => {
+    el.style.marginLeft = -current * 100 + "%";
+  });
+};
+
+// side menu links
+[].forEach.call(mainLinks, (link, index) => {
+  link.addEventListener("click", () => {
+    current = index;
+    swipe(current);
+  });
+});
+
+const swipe = direction => {
+  if (direction === "left") {
+    current <= 0 ? 0 : current--; // swipe left
+  } else if (direction === "right") {
+    current >= sections.length - 1 ? sections.length - 1 : current++;
+  } else {
+    current = direction;
+  }
+  swipeSection(current); // trigger swipe
+  animateCurrentText(current); // trigger text animation
+  hideMenu(); // hide menu if opened
+};
